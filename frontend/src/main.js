39f022f00dffb8a2cd9995eea76d1c8141e24d26
Artem Shuvaev <shuvaev13@mail.ru>
2019-09-05 05:02:48 +1000

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,9 @@ import Snotify, { SnotifyPosition } from "vue-snotify";
 import BootstrapVue from "bootstrap-vue";
 import VueRouter from "vue-router";
 import Vuex from "vuex";
-import Vue from 'vue'
+import Vue from 'vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import routes from "./routes.js";
 import App from "./App.vue";
